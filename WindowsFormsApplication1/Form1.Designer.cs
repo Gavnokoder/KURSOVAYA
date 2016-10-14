a0de1967048efebe5ce3b00c7d19cb7ae85a3176
Gavnokoder <Trashisreal@gmail.com>
2016-10-14 22:02:20 +0400

    ыы ебать

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Амперметр ",
+            "Вольтметр",
+            "Резистор",
+            "Реостат",
+            "Лампа",
+            "Диод",
+            "Провод",
+            "Конденсатор",
+            "Источник тока",
+            "Трансформатор",
+            "Предохранитель",
+            "Ключ",
+            "Катушка индукции"});
+            this.listBox1.Location = new System.Drawing.Point(37, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 184);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Tag = "";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseEnter += new System.EventHandler(this.listBox1_MouseEnter);
+            this.listBox1.MouseLeave += new System.EventHandler(this.listBox1_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(79, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Элементы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(443, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Электрическая схема";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(81, 298);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(83, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 484);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(206, 141);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 643);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Область";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 328);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 376);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 409);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(93, 282);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(879, 684);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Редактор электрических схем";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        int obj = 0;
+        int angle = 1;
+        string name = "";
+        ushort[,] dot = new ushort[17, 6];
+        List<PictureBox> area = new List<PictureBox>(50);
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+            this.MouseWheel += new MouseEventHandler(MouseWheelHandler);
+            //PictureBox[] area = new PictureBox[25];
+           // for (int i = 0; i < 25; i++) area[i] = new PictureBox();
+
+            for (int i = 0; i < 50; i++)
+            {
+                PictureBox area = new PictureBox();
+                area.Name = i.ToString();
+                area.Size = new Size(233, 20);
+            }
+        }
+
+       // private PictureBox[] area;
+
+        private void Form1_Shown(Object sender, EventArgs e)
+        {
+            // строим точки
+            Graphics lin;
+            lin = CreateGraphics();
+            Pen blackPen = new Pen(Color.FromArgb(150, 0, 0, 0), 2);
+            for (int i = 130; i < 600; i += 120)
+            {
+                for (int j = 330; j < 700; j += 120) lin.DrawEllipse(blackPen, j, i, 2, 2);
+            }
+            // строим линии
+            lin.DrawLine(blackPen, 270, 70, 270, 560);
+            lin.DrawLine(blackPen, 270, 560, 760, 560);
+            lin.DrawLine(blackPen, 760, 560, 760, 70);
+            lin.DrawLine(blackPen, 760, 70, 270, 70);
+            for (int i = 1; i < 17; i++) richTextBox1.Text += String.Format("{0} {1} {2}", i, Convert.ToString(dot[i, 1]), Convert.ToString(dot[i, 2]) + "\n");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Заполняем координаты x, y для каждой точки 
+            ushort x = 330;
+            ushort y = 130;
+            int l = 0;
+            for (int i = 1; i < 17; i++)
+            {
+                l++;
+                if (l > 4)
+                {
+                    l = 1;
+                    x = 330;
+                    y += 120;
+                }
+                dot[i, 1] = x;
+                dot[i, 2] = y;
+                x += 120;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            angle = 1;
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 1:
+                    name = "Вольтметр";
+                    pictureBox1.Load("Вольтметр1.jpg");
+                    obj = 2;
+                    break;
+                case 2:
+                    name = "Резистор";
+                    pictureBox1.Load("Резистор1.jpg");
+                    obj = 3;
+                    break;
+                case 3:
+                    name = "Реостат";
+                    pictureBox1.Load("Реостат1.jpg");
+                    obj = 4;
+                    break;
+                case 4:
+                    name = "Реостат";
+                    pictureBox1.Load("Реостат1.jpg");
+                    obj = 4;
+                    break;
+                case 5:
+                    name = "Реостат";
+                    pictureBox1.Load("Реостат1.jpg");
+                    obj = 4;
+                    break;
+                case 6:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 7:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 8:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 9:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 10:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 11:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+                case 12:
+                    name = "Амперметр";
+                    pictureBox1.Load("Амперметр1.jpg");
+                    obj = 1;
+                    break;
+            }
+        }
+
+        private void MouseWheelHandler(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (obj != 0)
+            {
+                if (e.Delta > 0)
+                {
+                    switch (angle)
+                    {
+                        case 1:
+                            angle = 2;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                        case 2:
+                            angle = 3;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                        case 3:
+                            angle = 4;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                        case 4:
+                            angle = 1;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (angle)
+                    {
+                        case 1:
+                            angle = 4;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                        case 2:
+                            angle = 1;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                        case 3:
+                            angle = 2;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                        case 4:
+                            angle = 3;
+                            pictureBox1.Load(name + angle + ".jpg");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void listBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+        private void listBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.ActiveControl = listBox1;
+        }
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            var pos = this.PointToClient(Cursor.Position);
+            int idx = 0;
+            int k = 0;
+            if (e.Button == MouseButtons.Left)
+            {
+                textBox1.Text = String.Format("({0}, {1})", pos.X, pos.Y);
+                // попадание в область
+                if (obj != 0)
+                {
+                    if (angle == 1 || angle == 3)
+                    {
+                        label9.Text = "HORIZON";
+                        for (int i = 1; i < 13; i++)
+                        {
+                            idx++;
+                            if (idx > 3)
+                            {
+                                k++;
+                                idx = 1;
+                            }
+                            if (Math.Abs(((dot[i + k + 1, 1] - dot[i + k, 1]) / 2) + dot[i + k, 1] - pos.X) <= 60 && Math.Abs(dot[i + k, 2] - pos.Y) <= 40)
+                            {
+                                label5.Text = Convert.ToString(dot[i + k, 1]);
+                                label6.Text = Convert.ToString(dot[i + k + 1, 1]);
+                                label4.Text = String.Format("Область {0}", i);
+                                label7.Text = Convert.ToString(k);
+                                label8.Text = Convert.ToString(idx);
+
+                                PictureBox picture  = new PictureBox();
+                                picture.Location = new Point(dot[i + k, 1] + 33, dot[i + k, 2] - 24);
+                                Size size = new Size(51, 49);
+                                picture.Size = size;
+                                this.Controls.Add(picture);
+
+                                for (int h = 0; h < angle + 1; h++) if (h == angle) picture.Load("Область_" + name + angle + ".jpg");
+
+                                PictureBox line2 = new PictureBox();
+                                line2.Location = new Point(dot[i + k, 1], dot[i + k, 2] - 2);
+                                Size size2 = new Size(122, 6);
+                                line2.Size = size2;
+                                this.Controls.Add(line2);
+                                line2.Load("Область_Линия1.jpg");
+
+                                //PictureBox snd = (PictureBox)sender;
+                                //int id = picture1.IndexOf(snd);
+                                //this.Controls.Remove(snd);
+                                this.Controls.Add(area[i]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        label9.Text = "VERTICAL";
+                        for (int i = 1; i < 13; i++)
+                        {
+
+                            if (Math.Abs(((dot[i + 4, 2] - dot[i, 2]) / 2) + dot[i, 2] - pos.Y) <= 50 && Math.Abs(dot[i + k, 1] - pos.X) <= 40)
+                            {
+                                label5.Text = Convert.ToString(dot[i + k, 2]);
+                                label6.Text = Convert.ToString(dot[i + k + 4, 2]);
+                                label4.Text = String.Format("Область {0}", i);
+                                label7.Text = Convert.ToString(k);
+                                label8.Text = Convert.ToString(idx);
+
+                                PictureBox picture = new PictureBox();
+                                picture.Location = new Point(dot[i, 1] - 24, dot[i, 2] + 35);
+                                Size size = new Size(49, 51);
+                                picture.Size = size;
+                                this.Controls.Add(picture);
+
+                                for (int h = 0; h < angle + 1; h++) if (h == angle) picture.Load("Область_" + name + angle + ".jpg");
+
+                                PictureBox line1 = new PictureBox();
+                                line1.Location = new Point(dot[i + k, 1] - 1, dot[i + k, 2]);
+                                Size size1 = new Size(6, 122);
+                                line1.Size = size1;
+                                this.Controls.Add(line1);
+                                line1.Load("Область_Линия2.jpg");
+
+                            }
+                        }
+                    }
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                if (angle == 1 || angle == 3)
+                {
+                    for (int i = 1; i < 13; i++)
+                    {
+                        idx++;
+                        if (idx > 3)
+                        {
+                            k++;
+                            idx = 1;
+                        }
+                        if (Math.Abs(((dot[i + k + 1, 1] - dot[i + k, 1]) / 2) + dot[i + k, 1] - pos.X) <= 60 && Math.Abs(dot[i + k, 2] - pos.Y) <= 40)
+                        {
+                            this.Controls.Add(area[0]);
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+        }
+    }
+    }
